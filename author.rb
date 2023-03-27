a52c03586105728e00e1d277b1ba2b22d8c8fdee
Allassane <dickoallassane1997@gmail.com>
2023-03-27 18:19:29 +0100

    Add author.rb class

---
+++ author.rb
@@ -0,0 +1,13 @@
+
+class Author
+  attr_reader :id
+  attr_accessor :first_name, :last_name, :items
+
+  def initialize(id, first_name, last_name)
+    @id = id,
+    @first_name = first_name,
+    @last_name = last_name
+    @items = []
+  end
+  
+end
