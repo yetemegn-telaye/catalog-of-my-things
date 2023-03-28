ff411d772393c145f3b502874a46fd189ade85b0
yetemegn-telaye <lemma19yet@gmail.com>
2023-03-28 17:24:10 +0300

    Create musicalbum class

---
+++ music_album.rb
@@ -0,0 +1,17 @@
+require_relative 'item.rb'
+
+class MusicAlbum < Item
+    attr_reader :on_spotify,:genre,:source,:label
+    def initialize(on_spotify,genre,publish_date,source,label)
+        super(publish_date)
+        @on_spotify=on_spotify
+        @genre=genre
+        @source=source
+        @label=label
+    end
+
+    def can_be_archived?
+        return true if(super && @on_spotify==true)
+        return false
+    end
+end
