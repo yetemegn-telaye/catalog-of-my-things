9c267dd49b058c01887e0d8c171553a8e07e4fc7
yetemegn-telaye <lemma19yet@gmail.com>
2023-03-28 12:56:52 +0300

    Create item class and its constructor

---
+++ item.rb
@@ -0,0 +1,23 @@
+require 'date'
+class Item
+    attr_reader :genre,:author,:source,:label,:id
+    attr_accessor :archived,:publish_date
+    def initialize(publish_date)
+        @id=rand(1..100)
+        @publish_date=publish_date
+        @archived=false
+    end
+    def set_genre(genre)
+        genre=genre
+    end
+    def set_author(author)
+        author=author
+    end
+    def set_source(source)
+        source=source
+    end
+    def set_label(label)
+        label=label
+    end
+
+end
