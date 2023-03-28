3800a832fd55cc232bcadd655a4d710a359f49ef
Allassane <dickoallassane1997@gmail.com>
2023-03-28 17:21:19 +0100

    Remove create, load_all and save_all method

--- author.rb
+++ author.rb
@@ -29,33 +29,4 @@ class Author
     end
   end
 
-  def self.create
-    puts "\nSelect the author information"
-    print "First Name: "
-    first_name = gets.chomp.to_s
-
-    print "Last Name: "
-    last_name = gets.chomp.to_s
-
-    puts "Author created succcessfully!"
-    author = new(first_name, last_name)
-  end
-
-  def self.save_all
-    if File.exist?("./data/authors.json")
-      list = []
-      all.each do |author|
-        list << {id: author.id, first_name: author.first_name, last_name: author.last_name }
-      end 
-      File.write("./data/authors.json", JSON.pretty_generate(list))
-    end
-  end
-  def self.load_all
-    return false if !File.exist?('./data/authors.json')
-    return false if File.read("./data/authors.json").empty?
-    list_authors = JSON.parse(File.read("./data/authors.json"))
-    list_authors.each do |author|
-      new(author["first_name"], author["last_name"], author["id"])
-    end
-  end
 end
--- game.rb
+++ game.rb
@@ -25,76 +25,8 @@ class Game < Item
     end
   end
 
-  def self.create
-    puts "\nSelect the Game information"
-
-    # Choose author
-    puts "Select the author by index (not id):"
-    Author.show_list
-    index = gets.chomp.to_i
-    list_author = Author.all
-    return if index < 0 || index > list_author.length
-    author = list_author[index]
-    author.add_item(self)
-
-    # todo
-    # choose genre
-    # choose label
-    # choose source
-
-    print "Publication date: "
-    publish_date = gets.chomp
-
-    print "Multiplayer [Y, N]: "
-    value = gets.chomp.to_s.upcase
-    multiplayer = false
-    multiplayer = true if value == "Y"
-
-    print "Last Played at: "
-    last_played_at = gets.chomp
-
-    game = new(publish_date, multiplayer, last_played_at)
-    game.add_author(author)
-    puts "Game created succcessfully!"
-    game
-  end
-
   def can_be_archived?
     super && (Date.today.year - Date.parse(@last_played_at).year) > 2
-  end
-
-  def self.save_all
-    return false if !File.exist?("./data/game.json")
-    list = []
-    all.each do |game|
-      #how to save the authors, the labels, the genre etc...
-      data = { id: game.id, author_id: game.author.id,
-        #source_id: @source&.id, label_id: @label&.id, genre_id: @genre.id, 
-        multiplayer: game.multiplayer, last_played_at: game.last_played_at 
-      }
-      list << data
-    end 
-    File.write("./data/game.json", JSON.pretty_generate(list))
-    true
-  end
-
-  def self.load_all
-    return false if !File.exist?('./data/game.json')
-    return false if File.read("./data/game.json").empty?
-    list = JSON.parse(File.read("./data/game.json"))
-    list.each do |data|
-      game = new(data["publish_date"], data["multiplayer"], data["last_played_at"], data["id"])
-      
-      # add authors
-      game_author = Author.all.find { |author| author.id == data["author_id"]}
-      game_author&.add_item(game)
-      game&.add_author(game_author)
-
-      # todo
-      # add label
-      # add source
-      # add genre
-    end
   end
 
 end
