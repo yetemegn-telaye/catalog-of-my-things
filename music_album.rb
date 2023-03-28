b10e02beb854a0b9242aee067131f7e77753dd4e
yetemegn-telaye <lemma19yet@gmail.com>
2023-03-28 17:35:39 +0300

    Fix linters errors

--- music_album.rb
+++ music_album.rb
@@ -1,17 +1,19 @@
-require_relative 'item.rb'
+require_relative 'item'
 
 class MusicAlbum < Item
-    attr_reader :on_spotify,:genre,:source,:label
-    def initialize(on_spotify,genre,publish_date,source,label)
-        super(publish_date)
-        @on_spotify=on_spotify
-        @genre=genre
-        @source=source
-        @label=label
-    end
+  attr_reader :on_spotify, :genre, :source, :label
 
-    def can_be_archived?
-        return true if(super && @on_spotify==true)
-        return false
-    end
+  def initialize(on_spotify, genre, publish_date, source, label)
+    super(publish_date)
+    @on_spotify = on_spotify
+    @genre = genre
+    @source = source
+    @label = label
+  end
+
+  def can_be_archived?
+    return true if super && @on_spotify == true
+
+    false
+  end
 end
