deada09dceec04e38282c9d1ed505a360abe929c
Allassane <dickoallassane1997@gmail.com>
2023-03-29 15:04:13 +0100

    Add test for game methods

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,48 +1,40 @@
-require '../author'
+require './author'
 
 describe Author do
-  context "#show_list" do
-    it "#show_list: empty" do
+  context '#show_list' do
+    it '#show_list: empty' do
       puts Author.show_list
-      expect(Author.show_list).to eq (puts 'No authors available')
+      expect(Author.show_list).to eq(puts('No authors available'))
     end
   end
 
-  context "test author basic method" do
+  context 'test author basic method' do
     before(:all) do
-      @author = Author.new("Dicko", "Allassane")
-      @game = Game.new("2000", false, "2010")
+      @author = Author.new('Dicko', 'Allassane')
     end
-    it "#initialize" do
+    it '#initialize' do
       expect(@author.id).to be_kind_of Integer
-      expect(@author.first_name).to eq "Dicko"
-      expect(@author.last_name).to eq "Allassane"
+      expect(@author.first_name).to eq 'Dicko'
+      expect(@author.last_name).to eq 'Allassane'
     end
 
-    it "#to_s" do
-      expect("#{@author}").to eq "Dicko Allassane"
+    it '#to_s' do
+      expect(@author.to_s).to eq 'Dicko Allassane'
     end
 
-    it "Author.all" do
+    it 'Author.all' do
       expect(Author.all).to contain_exactly(@author)
-      author2 = Author.new("Trast", "trast00")
+      author2 = Author.new('Trast', 'trast00')
       expect(Author.all).to contain_exactly(@author, author2)
     end
   end
 
-  context "#add_item" do
+  context '#add_item' do
     before(:all) do
-      @author = Author.new("Dicko", "Allassane")
-      @game = Game.new("2000", false, "2010")
+      @author = Author.new('Dicko', 'Allassane')
     end
-    it "empty list of item" do
+    it 'empty list of item' do
       expect(@author.items).to eq []
     end
-
-    it "empty list of item" do
-      @author.add_item(@game)
-      expect(@author.items).to eq [@game]
-    end
   end
-
 end
