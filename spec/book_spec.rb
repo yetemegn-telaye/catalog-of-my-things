832461bbc34bcfac48e727a53b82fbc5a6bacd9b
Dmambo <mambotrader0@gmail.com>
2023-03-29 23:10:53 +0530

    Added unit test for my book and label classes

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -4,57 +4,57 @@ require 'date'
 
 
 describe Book do
-  let(:title) { "The Great Gatsby" }
-  let(:publish_date) { "1925-04-10" }
+  let(:title) { 'The Great Gatsby' }
+  let(:publish_date) { '1925-04-10' }
   let(:publisher) { "Charles Scribner's Sons" }
-  let(:cover_state) { "good" }
+  let(:cover_state) { 'good' }
   subject(:book) { described_class.new(title, publish_date, publisher, cover_state) }
 
-  describe "#initialize" do
-    it "sets the title" do
+  describe '#initialize' do
+    it 'sets the title' do
       expect(book.title).to eq(title)
     end
 
-    it "sets the publish date" do
+    it 'sets the publish date' do
       expect(book.publish_date).to eq((publish_date))
     end
 
-    it "sets the publisher" do
+    it 'sets the publisher' do
       expect(book.publisher).to eq(publisher)
     end
 
-    it "sets the cover state" do
+    it 'sets the cover state' do
       expect(book.cover_state).to eq(cover_state)
     end
   end
 
-  describe "#add_author" do
-    it "adds an author to the book" do
-      author = "F. Scott Fitzgerald"
+  describe '#add_author' do
+    it 'adds an author to the book' do
+      author = 'F. Scott Fitzgerald'
       book.add_author(author)
       expect(book.author).to include(author)
     end
   end
 
-  describe "#can_be_archived?" do
-    context "when the book is archived" do
+  describe '#can_be_archived?' do
+    context 'when the book is archived' do
       before { book.archived = true }
 
-      it "returns true" do
+      it 'returns true' do
         expect(book.can_be_archived?).to be(true)
       end
     end
 
-    context "when the cover state is bad" do
-      let(:cover_state) { "bad" }
+    context 'when the cover state is bad' do
+      let(:cover_state) { 'bad' }
 
-      it "returns true" do
+      it 'returns true' do
         expect(book.can_be_archived?).to be(true)
       end
     end
 
-    context "when the book is not archived and the cover state is good" do
-      it "returns false" do
+    context 'when the book is not archived and the cover state is good' do
+      it 'returns false' do
         expect(book.can_be_archived?).to be(true)
       end
     end
