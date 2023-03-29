77d445285abb6ef201c74acd3aa9d40a69c7ce93
Allassane <dickoallassane1997@gmail.com>
2023-03-29 21:47:59 +0100

    Add querie.sql

--- database/data.sql
+++ database/data.sql
@@ -1,3 +1,11 @@
+/* authors */
+INSERT into authors values (DEFAULT, 'Allassane', 'Dicko');
+INSERT into authors values (DEFAULT, 'Traster', 'Trast');
 
-INSERT into items (publish_date, archived, author_id) values ("1999", false)
-INSERT into items (publish_date, archived, author_id) values ("2000", false)
+/* Items */
+INSERT into items(id, publish_date, archived, author_id) values (1, '2020-01-01', false, 1);
+INSERT into items (id, publish_date, archived, author_id) values (2, '2000-01-01', false, 2);
+
+/* games */
+INSERT into games (item_id, multiplayer, last_played_at) values (0, true, '2006-01-01');
+INSERT into games (item_id, multiplayer, last_played_at) values (1, true, '2010-01-01');
