d0bfd4015f8db8de3649fef2ec2d30c12ac97f48
yetemegn-telaye <lemma19yet@gmail.com>
2023-03-30 13:51:12 +0300

    Fix linters

--- storage.rb
+++ storage.rb
@@ -3,7 +3,6 @@ require 'json'
 class Storage
   MUSIC_ALBUM_FILE = './data/music_album.json'.freeze
   GENRES_FILE = './data/genre.json'.freeze
-
   BOOKS_FILE = './data/books.json'.freeze
   LABELS_FILE = './data/labels.json'.freeze
 
@@ -50,39 +49,29 @@ class Storage
     file_data.each do |data|
       music_album = MusicAlbum.new(data['title'], data['on_spotify'], data['publish_date'])
       music_albums << music_album
-
+    end
     File.write(BOOKS_FILE, JSON.pretty_generate(@app.books.map(&:to_h)))
     File.write(LABELS_FILE, JSON.pretty_generate(@app.labels.map(&:to_h)))
   end
 
-  private
+  #   def read_data(filename)
+  #     return [] unless File.exist?(filename)
 
-  def read_data(filename)
-    return [] unless File.exist?(filename)
+  #     file = File.open(filename)
+  #     file_data = file.read
+  #     file.close
+  #     return JSON.parse(file_data) unless file_data.empty?
 
-    file = File.open(filename)
-    file_data = file.read
-    file.close
-    return JSON.parse(file_data) unless file_data.empty?
+  #     []
+  #   end
 
-    []
-  end
-
-  def load_genres(filename, genres)
-    file_data = read_data(filename)
-    file_data.each do |data|
-      genre = Genre.new(data['name'])
-      genres << genre
-    end
-  end
-
-  def load_music_albums(filename, music_albums)
-    file_data = read_data(filename)
-    file_data.each do |data|
-      music_album = MusicAlbum.new(data['title'], data['on_spotify'], data['publish_date'])
-      music_albums << music_album
-    end
-  end
+  #   def load_genres(filename, genres)
+  #     file_data = read_data(filename)
+  #     file_data.each do |data|
+  #       genre = Genre.new(data['name'])
+  #       genres << genre
+  #     end
+  #   end
 
   def load_books(filename, books)
     file_data = read_data(filename)
